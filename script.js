e76c3a693a4a28442d47bea65ca050faea4f45cd
AdrianFurquim <adrian.furquim26@gmail.com>
2024-06-26 14:37:29 -0300

    Adicionando Academy Training

---
+++ script.js
@@ -0,0 +1,60 @@
+let table_segunda = document.getElementById("table_segunda");
+let table_terca = document.getElementById("table_terca");
+let table_quarta = document.getElementById("table_quarta");
+let table_quinta = document.getElementById("table_quinta");
+let table_sexta = document.getElementById("table_sexta");
+let table_sabado = document.getElementById("table_sabado");
+
+function segunda(){
+    table_segunda.style.display = "block";
+    table_terca.style.display = "none";
+    table_quarta.style.display = "none";
+    table_quinta.style.display = "none";
+    table_sexta.style.display = "none";
+    table_sabado.style.display = "none";
+}
+
+function terca(){
+    table_segunda.style.display = "none";
+    table_terca.style.display = "block";
+    table_quarta.style.display = "none";
+    table_quinta.style.display = "none";
+    table_sexta.style.display = "none";
+    table_sabado.style.display = "none";
+}
+
+function quarta(){
+    table_segunda.style.display = "none";
+    table_terca.style.display = "none";
+    table_quarta.style.display = "block";
+    table_quinta.style.display = "none";
+    table_sexta.style.display = "none";
+    table_sabado.style.display = "none";
+}
+
+function quinta(){
+    table_segunda.style.display = "none";
+    table_terca.style.display = "none";
+    table_quarta.style.display = "none";
+    table_quinta.style.display = "block";
+    table_sexta.style.display = "none";
+    table_sabado.style.display = "none";
+}
+
+function sexta(){
+    table_segunda.style.display = "none";
+    table_terca.style.display = "none";
+    table_quarta.style.display = "none";
+    table_quinta.style.display = "none";
+    table_sexta.style.display = "block";
+    table_sabado.style.display = "none";
+}
+
+function sabado(){
+    table_segunda.style.display = "none";
+    table_terca.style.display = "none";
+    table_quarta.style.display = "none";
+    table_quinta.style.display = "none";
+    table_sexta.style.display = "none";
+    table_sabado.style.display = "block";
+}
